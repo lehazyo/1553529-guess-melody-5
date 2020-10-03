06a4a4fa10664e67cd08736a784d617a09f71712
Алексей Сергиенко <lehazyo@gmail.com>
2020-10-03 16:17:03 +0300

    Урок 1 задание 4 / Коммит 2

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import App from './components/App/App';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+    <App
+      errorsCount={3}
+    />,
+    document.getElementById(`root`)
+);
